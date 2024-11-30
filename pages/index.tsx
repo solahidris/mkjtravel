f31ffa310bd9416493a7f8fdb7107f6d72c91af8
solahidris <emailsolah@gmail.com>
2024-11-30 20:30:05 +0900

    done connecting

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,7 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { PiCloudSunBold } from "react-icons/pi";
-import { FaPerson, FaChild,FaBabyCarriage } from "react-icons/fa6";
-
+import { FaPerson, FaChild, FaBabyCarriage } from "react-icons/fa6";
 
 type ItineraryData = {
   guideName: string;
@@ -15,6 +14,8 @@ type ItineraryData = {
   dateStart: string;
   dateEnd: string;
   itenaryDetails: { [key: string]: string };
+  accomodation: string; // Add this line
+  tickets: string; // Add this line
 };
 
 export default function PDFPage() {
@@ -33,9 +34,22 @@ export default function PDFPage() {
     return (
       <div className="flex gap-4">
         <div className="flex gap-2 font-medium">
-          <div className="flex items-center gap-0.5">{`${adults}`}<FaPerson className="w-4 h-4"/></div>
-          {children > 0 && <div className="flex items-center gap-0.5">{` ${children}`}<FaChild className="w-4 h-4"/></div>}
-          {infants > 0 && <div className="flex items-center gap-2">{` ${infants}`}<FaBabyCarriage className="w-4 h-4"/></div>}
+          <div className="flex items-center gap-0.5">
+            {`${adults}`}
+            <FaPerson className="w-4 h-4" />
+          </div>
+          {children > 0 && (
+            <div className="flex items-center gap-0.5">
+              {` ${children}`}
+              <FaChild className="w-4 h-4" />
+            </div>
+          )}
+          {infants > 0 && (
+            <div className="flex items-center gap-2">
+              {` ${infants}`}
+              <FaBabyCarriage className="w-4 h-4" />
+            </div>
+          )}
         </div>
         {`(${totalPax} pax)`}
       </div>
@@ -45,8 +59,8 @@ export default function PDFPage() {
   // Helper function to format date as DD/MM
   const formatDate = (dateString: string) => {
     const date = new Date(dateString);
-    const day = date.getDate().toString().padStart(2, '0');
-    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, "0");
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
     return `${day}/${month}`;
   };
 
@@ -73,8 +87,11 @@ export default function PDFPage() {
 
   const startDate = new Date(data.dateStart);
   const endDate = new Date(data.dateEnd);
-  const numberOfDays = Math.ceil((endDate.getTime() - startDate.getTime()) / (1000 * 3600 * 24))+1;
+  const numberOfDays =
+    Math.ceil((endDate.getTime() - startDate.getTime()) / (1000 * 3600 * 24)) +
+    1;
 
+  console.log("data.tickets:", data.tickets);
 
   return (
     <div className="overflow-hidden">
@@ -98,9 +115,7 @@ export default function PDFPage() {
         </div>
 
         <div className="pt-10 flex flex-col gap-12">
-          <h1 className="uppercase font-extrabold text-5xl">
-            Guide Guideline
-          </h1>
+          <h1 className="uppercase font-extrabold text-5xl">Guide Guideline</h1>
           <div className="flex flex-col rounded-lg p-4 border shadow-md bg-white border-gray-300">
             <div className="grid grid-cols-4">
               <strong className="col-span-1 border-r mr-4">Guide Name</strong>
@@ -117,7 +132,7 @@ export default function PDFPage() {
                 <div className="flex gap-1 items-center">
                   <p>{`(`}</p>
                   <PiCloudSunBold />
-                  <p>{`${numberOfDays}H${numberOfDays-1}M )`}</p>
+                  <p>{`${numberOfDays}H${numberOfDays - 1}M )`}</p>
                 </div>
               </div>
             </div>
@@ -129,14 +144,39 @@ export default function PDFPage() {
               <strong className="col-span-1 border-r mr-4">
                 Flight Detail
               </strong>
-              <a
+              {/* <a
                 href={data.flightLink}
                 className="col-span-3 text-blue-700 underline"
-              >{`Flight detail - ${data.clientName} (link)`}</a>
+              >{`Flight detail - ${data.clientName} (link)`}</a> */}
+              {/* <p className="col-span-3">{data.flightLink}</p> */}
+              <div className="col-span-3">
+                {data.flightLink.split(", ").map((flight, index) => {
+                  const match = flight.match(/(.+?) \((https?:\/\/.+?)\)/);
+                  if (match) {
+                    const [ name, link] = match;
+                    return (
+                      <div key={index}>
+                        <span>{`${name} `}</span>
+                        <a
+                          href={link}
+                          className="text-blue-700 underline"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                        {`( link )`}
+                        </a>
+                      </div>
+                    );
+                  }
+                  return null;
+                })}
+              </div>
+              
             </div>
           </div>
         </div>
 
+        {/* Client Remarks */}
         <div className="mt-10 flex flex-col rounded-lg p-4 border shadow-md bg-white border-gray-300">
           <p className="text-center font-bold border-b pb-4">Client Remark</p>
           <div
@@ -145,12 +185,34 @@ export default function PDFPage() {
           />
         </div>
 
+        {/* Tickets */}
         <div className="mt-10 flex flex-col rounded-lg p-4 border shadow-md bg-white border-gray-300">
           <p className="text-center font-bold border-b pb-4">Tickets</p>
-          <div
-            dangerouslySetInnerHTML={{ __html: data.remarks }}
-            className="leading-5 pt-4 text-sm"
-          />
+          {/* <p className="leading-5 pt-4 text-sm">{data.tickets}</p> */}
+          <div className="leading-5 pt-4 text-sm">
+            {data.tickets.split(', ').map((ticket, index) => {
+              console.log(`Processing ticket: ${ticket}`); // Debugging line
+              const match = ticket.match(/(.+?) \((.+?)\)/);
+              if (match) {
+                const [ name, link] = match;
+                return (
+                  <div key={index}>
+                    <span>{`${name} `}</span>
+                    <a
+                      href={link}
+                      className="text-blue-700 underline"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {`( link )`}
+                    </a>
+                  </div>
+                );
+              }
+              return null; // Skip entries that don't match
+            })}
+          </div>
+
         </div>
 
         <div className="mt-10 flex flex-col rounded-lg p-4 border shadow-md bg-white border-gray-300">
@@ -160,26 +222,58 @@ export default function PDFPage() {
               Object.entries(data.itenaryDetails).map(([key, value], index) => {
                 const currentDate = addDays(startDate, index);
                 return (
-                  <div key={key} className={`flex ${index !== 0 && "border-t pt-4"}`}>
+                  <div
+                    key={key}
+                    className={`flex ${index !== 0 && "border-t pt-4"}`}
+                  >
                     <div className="flex flex-col pr-4 border-r">
                       <strong>Day {index + 1}</strong>
-                      <p className="text-[10px]">{`${formatDate(currentDate.toISOString())}`}</p>
+                      <p className="text-[10px]">{`${formatDate(
+                        currentDate.toISOString()
+                      )}`}</p>
                     </div>
                     <p className="pl-4">{value as string}</p>
                   </div>
                 );
               })}
-            </div>
+          </div>
         </div>
 
-        <div className="mt-10 flex flex-col rounded-lg p-4 border shadow-md bg-white border-gray-300">
+        {/* <div className="mt-10 flex flex-col rounded-lg p-4 border shadow-md bg-white border-gray-300">
           <p className="text-center font-bold border-b pb-4">Accomodation</p>
           <div
             dangerouslySetInnerHTML={{ __html: data.remarks }}
             className="leading-5 pt-4 text-sm"
           />
+        </div> */}
+        {/* Accomodation */}
+        <div className="mt-10 flex flex-col rounded-lg p-4 border shadow-md bg-white border-gray-300">
+          <p className="text-center font-bold border-b pb-4">Accomodation</p>
+          <p className="leading-5 pt-4 text-sm">{data.accomodation}</p>
+          <div className="leading-5 pt-4 text-sm">
+            {data.accomodation.split(', ').map((accomodation, index) => {
+              console.log(`Processing ticket: ${accomodation}`); // Debugging line
+              const match = accomodation.match(/(.+?) \((.+?)\)/);
+              if (match) {
+                const [ name, link] = match;
+                return (
+                  <div key={index}>
+                    <span>{`${name} `}</span>
+                    <a
+                      href={link}
+                      className="text-blue-700 underline"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {`( link )`}
+                    </a>
+                  </div>
+                );
+              }
+              return null; // Skip entries that don't match
+            })}
+          </div>
         </div>
-
       </div>
     </div>
   );
